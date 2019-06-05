77927d6daf48da17970e7968fc669e954b9e5a3a
Krzysztof Krysiak <krysiak86@gmail.com>
2019-06-05 13:52:29 +0200

    Add vue complete app (todo)

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <header class="header">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/todo-app">TodoApp</router-link>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
+</style>
---
+++ src/views/TodoApp.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="todoApp">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from '@/components/Todos'
+import AddTodo from '@/components/AddTodo'
+import axios from 'axios'
+
+export default {
+  name: 'TodoApp',
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
+      // this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+      // this.todos = [...this.todos, newTodo];
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+}
+</script>
+
+<style scoped>
+</style>
